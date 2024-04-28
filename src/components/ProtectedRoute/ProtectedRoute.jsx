15b30d2cccf5909067a4acd9dd0a3bddd66589c4
BiploHossainSobuj <bhs.cse@outlook.com>
2024-04-28 09:22:02 +0600

    added loader for private route

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -3,11 +3,11 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
 
 const ProtectedRoute = ({children}) => {
-    const {user,loading} = useContext(AuthContext);
+    const {user,loader} = useContext(AuthContext);
     const location = useLocation();
     console.log(location);
-    if(loading){
-        <span>loading</span>
+    if(loader){
+        return <span className="loading loading-dots loading-lg"></span>;
     }
     if (user) {
         return children;
