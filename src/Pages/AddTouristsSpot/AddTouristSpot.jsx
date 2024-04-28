323c7b2d436a4bbe5a02d1546b3aac17102cf94b
BiploHossainSobuj <bhs.cse@outlook.com>
2024-04-28 06:00:22 +0600

    added simple post method

--- src/Pages/AddTouristsSpot/AddTouristSpot.jsx
+++ src/Pages/AddTouristsSpot/AddTouristSpot.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const AddTouristSpot = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const user = { name, email };
+        fetch('http://localhost:5000/users',{
+            method: 'POST',
+            headers:{
+                'content-type':'application/json',
+            },
+            body:JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+        })
+    }
     return (
         <div>
-            AddTouristSpot
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' />
+                <input type="email" name='email' />
+                <input type="submit" value='add' />
+            </form>
         </div>
     );
 };
