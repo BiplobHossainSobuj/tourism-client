e1dd943c45712d781fdb78aecf88459389003c39
BiploHossainSobuj <bhs.cse@outlook.com>
2024-04-28 16:50:30 +0600

    read data from database in mylist route

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -3,10 +3,20 @@ import { AuthContext } from '../../context/AuthProvider';
 import { useLoaderData } from 'react-router-dom';
 
 const MyList = () => {
-    
+    const {user} = useContext(AuthContext);
+    const [spots,setSpots] = useState([]);
+    console.log(user)
+    useEffect(()=>{
+        fetch(`http://localhost:5000/mylist/${user.email}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            setSpots(data);
+        })
+    },[user])
     return (
         <div>
-           my list
+           my list:{spots.map(spt=>spt.tourists_spot_name)}
         </div>
     );
 };
