d5842b10ee3ff3470b2abeab16dce4d6070a8695
BiploHossainSobuj <bhs.cse@outlook.com>
2024-04-29 17:55:25 +0600

    added sort functionality

--- src/Pages/AllTouristsSpot/AllTouristsSpot.jsx
+++ src/Pages/AllTouristsSpot/AllTouristsSpot.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import TouristSpotCard from '../../components/TouristSpotCard/TouristSpotCard';
 
 const AllTouristsSpot = () => {
     const touristSpotDetails = useLoaderData();
-    const [touristSpots,setTouristSpots] = useState(touristSpotDetails);
+    const [touristSpots, setTouristSpots] = useState(touristSpotDetails);
+    const handleSort = ()=>{
+        const filtered = touristSpots.filter(spot=>spot.average_cost);
+        const sorted = filtered.sort((a,b)=>a.average_cost-b.average_cost);
+        setTouristSpots(sorted);
+    }
     return (
-        <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 container mx-auto'>
-            {
-                touristSpots.map(spot=><TouristSpotCard key={spot._id} spot={spot}></TouristSpotCard>)
-            }
+        <div className='container mx-auto'>
+            <button onClick={handleSort} className="btn btn-outline">
+                SORT
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+            </button>
+            <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+                {
+                    touristSpots.map(spot => <TouristSpotCard key={spot._id} spot={spot}></TouristSpotCard>)
+                }
+            </div>
         </div>
     );
 };
